99bfbca8d3d797aadf603c19dc0203719c349bae
SomyaGupta2608 <somyag2608@gmail.com>
2026-04-10 13:41:40 +0530

    This is UC2

--- IdeaProjects/HelloApp2/src/main/java/org/example/Hello.java
+++ IdeaProjects/HelloApp2/src/main/java/org/example/Hello.java
@@ -2,24 +2,7 @@ package org.example;
 public class Hello {
     public static void main(String[] args) {
 
-        // If no arguments are provided
-        if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            boolean first = true;
-
-            // Enhanced for loop to iterate through arguments
-            for (String name : args) {
-                if (!first) {
-                    nameBuilder.append(", ");
-                }
-                nameBuilder.append(name);
-                first = false;
-            }
-
-            // Print final output
-            System.out.println("Hello, " + nameBuilder.toString() + "!");
-        }
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
 }
